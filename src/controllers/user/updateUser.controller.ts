0f2c1024ac10b46b1176a48ded55378e2ada4bdc
Enrique <enrique.barbosasilva@gmail.com>
2022-10-18 18:28:06 -0300

    refactor: code-changed

--- src/controllers/user/updateUser.controller.ts
+++ src/controllers/user/updateUser.controller.ts
@@ -3,23 +3,15 @@ import { IUserUpdate } from "../../interfaces/users";
 import updateUser from "../../services/user/updateUser";
 
 const updateUserController = async (req: Request, res: Response) => {
-  try {
-    const user: IUserUpdate = req.body;
-    const id: string = req.params.id;
+  const user: IUserUpdate = req.body;
+  const id: string = req.params.id;
 
-    const updatedUser = await updateUser(user, id);
+  const updatedUser = await updateUser(user, id);
 
-    return res.status(200).json({
-      message: "User updated",
-      user: updatedUser,
-    });
-  } catch (error) {
-    if (error instanceof Error) {
-      return res.status(400).json({
-        message: error.message,
-      });
-    }
-  }
+  return res.status(200).json({
+    message: "User updated",
+    user: updatedUser,
+  });
 };
 
 export default updateUserController;
