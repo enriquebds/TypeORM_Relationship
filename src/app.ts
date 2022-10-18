bceb185475fed39286132019e3760830a286e716
Enrique <enrique.barbosasilva@gmail.com>
2022-10-18 16:11:14 -0300

    refactor: code-changed

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,11 @@ import "reflect-metadata";
 import "express-async-errors";
 import express from "express";
 import userRoutes from "./routes/user.routes";
+import loginRoute from "./routes/login.routes";
 
 const app = express();
 app.use(express.json());
-
+app.use("/login", loginRoute);
 app.use("/users", userRoutes);
 
 export default app;
