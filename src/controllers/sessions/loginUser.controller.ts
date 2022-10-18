35ac3ac1ce0fde77b5fbd15bb7be87d758f512b4
Enrique <enrique.barbosasilva@gmail.com>
2022-10-18 18:27:34 -0300

    refactor: code-changed

--- src/controllers/sessions/loginUser.controller.ts
+++ src/controllers/sessions/loginUser.controller.ts
@@ -3,18 +3,10 @@ import { IUserLogin } from "../../interfaces/users";
 import loginUser from "../../services/login/loginUser";
 
 const loginUserController = async (req: Request, res: Response) => {
-  try {
-    const data: IUserLogin = req.body;
-    const token = await loginUser(data);
+  const data: IUserLogin = req.body;
+  const token = await loginUser(data);
 
-    return res.json({ token });
-  } catch (error) {
-    if (error instanceof Error) {
-      res.status(403).json({
-        message: error.message,
-      });
-    }
-  }
+  return res.json({ token });
 };
 
 export default loginUserController;
