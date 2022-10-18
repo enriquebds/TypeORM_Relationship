d4069d73dc2cf00d0642e7c7341c5f433f4f0d55
Enrique <enrique.barbosasilva@gmail.com>
2022-10-18 16:03:31 -0300

    refactor: code-changed

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
-import "reflect-metadata"
-import "express-async-errors"
-import express from "express"
+import "reflect-metadata";
+import "express-async-errors";
+import express from "express";
+import userRoutes from "./routes/user.routes";
 
+const app = express();
+app.use(express.json());
 
-const app = express()
-app.use(express.json())
+app.use("/users", userRoutes);
 
-
-export default app
+export default app;
