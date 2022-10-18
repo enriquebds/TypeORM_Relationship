261a7c6dcfa5315cb2a6ea39e550c92cab62985d
Enrique <enrique.barbosasilva@gmail.com>
2022-10-18 13:04:12 -0300

    feat: folder-migrations-and-entities-created

---
+++ src/entities/user.entity.ts
@@ -0,0 +1,38 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import { Exclude } from "class-transformer";
+
+@Entity("users")
+class User {
+  @Column({ length: 60 })
+  name: string;
+
+  @Column({ length: 60, unique: true })
+  email: string;
+
+  @Column({ length: 120 })
+  @Exclude()
+  password: string;
+
+  @Column()
+  isAdm: boolean;
+
+  @Column({ default: true })
+  isActive: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+}
+
+export { User };
