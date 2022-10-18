2233667c0535a8035e16e6ab067ca2bca3f6539c
Enrique <enrique.barbosasilva@gmail.com>
2022-10-18 16:59:49 -0300

    feat: listUser-route-created

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import createUserController from "../controllers/user/createUser.controller";
+import listUsersController from "../controllers/user/listUsers.controller";
+import tokenAuthMiddleware from "../middlewares/tokenAuth.middleware";
+import verifyIsAdmMiddleware from "../middlewares/verifyIsAdm.middleware";
 
 const routes = Router();
 
 routes.post("", createUserController);
+routes.get("", tokenAuthMiddleware, verifyIsAdmMiddleware, listUsersController);
 
 export default routes;
