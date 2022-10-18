202af7ad370fe533208b0c13f35667a9feccab3e
Enrique <enrique.barbosasilva@gmail.com>
2022-10-18 17:08:48 -0300

    feat: deleteUser-route-created

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import createUserController from "../controllers/user/createUser.controller";
+import deleteUserController from "../controllers/user/deleteUser.controller";
 import listUsersController from "../controllers/user/listUsers.controller";
 import tokenAuthMiddleware from "../middlewares/tokenAuth.middleware";
 import verifyIsAdmMiddleware from "../middlewares/verifyIsAdm.middleware";
@@ -8,5 +9,11 @@ const routes = Router();
 
 routes.post("", createUserController);
 routes.get("", tokenAuthMiddleware, verifyIsAdmMiddleware, listUsersController);
+routes.delete(
+  "/:id",
+  tokenAuthMiddleware,
+  verifyIsAdmMiddleware,
+  deleteUserController
+);
 
 export default routes;
