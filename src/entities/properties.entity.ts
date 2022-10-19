38a201db6a520658aa1935dc974db0a5b0ef6c19
Enrique <enrique.barbosasilva@gmail.com>
2022-10-19 17:03:26 -0300

    feat: propertie-entity-created

---
+++ src/entities/properties.entity.ts
@@ -0,0 +1,37 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToOne,
+  JoinColumn,
+} from "typeorm";
+import { Addresses } from "./addresses.entity";
+
+@Entity("properties")
+class Properties {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column({ default: false })
+  sold: boolean;
+
+  @Column({ type: "decimal", precision: 12, scale: 2 })
+  value: number;
+
+  @Column({ type: "integer" })
+  size: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @OneToOne(() => Addresses)
+  @JoinColumn()
+  address: Addresses;
+}
+
+export { Properties };
