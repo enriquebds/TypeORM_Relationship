44d1cccc4a19ff095f3df405dc8113d3f73b547a
Enrique <enrique.barbosasilva@gmail.com>
2022-10-19 18:04:20 -0300

    refactor: code-changed

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,13 @@ import "express-async-errors";
 import handleErrorMiddleware from "./middlewares/handleError.middleware";
 import userRoutes from "./routes/user.routes";
 import loginRoute from "./routes/login.routes";
+import categoriesRoutes from "./routes/categories.routes";
 
 const app = express();
 app.use(express.json());
 app.use("/login", loginRoute);
 app.use("/users", userRoutes);
+app.use("/categories", categoriesRoutes);
 
 app.use(handleErrorMiddleware);
 export default app;
