b11fcf016e838d698be7b65c6c4e9b841ace4dc5
Enrique <enrique.barbosasilva@gmail.com>
2022-10-21 15:19:44 -0300

    refactor: code-changed

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import createCategoryController from "../controllers/categories/createCategory.controller";
+import listCategoryController from "../controllers/categories/listCategory.controller";
+import tokenAuthMiddleware from "../middlewares/tokenAuth.middleware";
 
 const routes = Router();
 
-routes.post("", createCategoryController);
+routes.post("", tokenAuthMiddleware, createCategoryController);
+routes.get("", listCategoryController);
 
 export default routes;
