e7155c7f755a18a2b67cdebfed6efe91b1acfa6b
Enrique <enrique.barbosasilva@gmail.com>
2022-10-21 15:20:22 -0300

    refactor: code-changed

--- src/migrations/1666296071205-createCategory.ts
+++ src/migrations/1666296071205-createCategory.ts
@@ -1,18 +1,25 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class createCategory1666296071205 implements MigrationInterface {
-    name = 'createCategory1666296071205'
+  name = "createCategory1666296071205";
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "categories" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying(60) NOT NULL, CONSTRAINT "PK_24dbc6126a28ff948da33e97d3b" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`ALTER TABLE "properties" ADD "categoryId" uuid`);
-        await queryRunner.query(`ALTER TABLE "properties" ADD CONSTRAINT "FK_a82b56d3d456c30b8c630cba0c6" FOREIGN KEY ("categoryId") REFERENCES "categories"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "categories" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying(60) NOT NULL, CONSTRAINT "PK_24dbc6126a28ff948da33e97d3b" PRIMARY KEY ("id"))`
+    );
+    await queryRunner.query(`ALTER TABLE "properties" ADD "categoryId" uuid`);
+    await queryRunner.query(
+      `ALTER TABLE "properties" ADD CONSTRAINT "FK_a82b56d3d456c30b8c630cba0c6" FOREIGN KEY ("categoryId") REFERENCES "categories"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "properties" DROP CONSTRAINT "FK_a82b56d3d456c30b8c630cba0c6"`);
-        await queryRunner.query(`ALTER TABLE "properties" DROP COLUMN "categoryId"`);
-        await queryRunner.query(`DROP TABLE "categories"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "properties" DROP CONSTRAINT "FK_a82b56d3d456c30b8c630cba0c6"`
+    );
+    await queryRunner.query(
+      `ALTER TABLE "properties" DROP COLUMN "categoryId"`
+    );
+    await queryRunner.query(`DROP TABLE "categories"`);
+  }
 }
