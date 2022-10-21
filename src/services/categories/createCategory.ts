aa06e219ddbe52ad9643464344487b64378f8331
Enrique <enrique.barbosasilva@gmail.com>
2022-10-20 23:38:01 -0300

    feat: code-added

--- src/services/categories/createCategory.ts
+++ src/services/categories/createCategory.ts
@@ -1,1 +1,39 @@
 import AppDataSource from "../../data-source";
+import { Category } from "../../entities/categories.entity";
+import { Property } from "../../entities/properties.entity";
+import { AppError } from "../../errors/appError";
+import { ICategoryRequest } from "../../interfaces/categories";
+
+const createCategory = async (
+  { name }: ICategoryRequest,
+  id: string
+): Promise<Category> => {
+  const categoryRepository = AppDataSource.getRepository(Category);
+  const propertyRepository = AppDataSource.getRepository(Property);
+
+  const property = await propertyRepository.findOneBy({
+    id,
+  });
+
+  const categoryExists = await categoryRepository.findOneBy({
+    name,
+  });
+
+  if (categoryExists) {
+    throw new AppError("Category already exists", 400);
+  }
+
+  if (!name) {
+    throw new AppError("Name is missing", 400);
+  }
+
+  const category = categoryRepository.create({
+    name,
+    property: property!,
+  });
+  await categoryRepository.save(category);
+
+  return category;
+};
+
+export default createCategory;
