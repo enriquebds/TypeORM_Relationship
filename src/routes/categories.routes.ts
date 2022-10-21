57974edd947c91d08d8b5175afcc7c0027fd6d26
Enrique <enrique.barbosasilva@gmail.com>
2022-10-20 23:39:14 -0300

    feat: code-added

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import createCategoryController from "../controllers/categories/createCategory.controller";
 
 const routes = Router();
+
+routes.post("", createCategoryController);
 
 export default routes;
