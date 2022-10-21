efdd00d896b4f2f74b8f1179d81d6d49f759ab2b
Enrique <enrique.barbosasilva@gmail.com>
2022-10-21 15:18:43 -0300

    refactor: code-changed

--- src/services/categories/createCategory.ts
+++ src/services/categories/createCategory.ts
@@ -1,35 +1,26 @@
 import AppDataSource from "../../data-source";
-import { Category } from "../../entities/categories.entity";
-import { Property } from "../../entities/properties.entity";
 import { AppError } from "../../errors/appError";
+import { Category } from "../../entities/categories.entity";
 import { ICategoryRequest } from "../../interfaces/categories";
 
-const createCategory = async (
-  { name }: ICategoryRequest,
-  id: string
-): Promise<Category> => {
+const createCategory = async ({
+  name,
+}: ICategoryRequest): Promise<Category> => {
   const categoryRepository = AppDataSource.getRepository(Category);
-  const propertyRepository = AppDataSource.getRepository(Property);
-
-  const property = await propertyRepository.findOneBy({
-    id,
-  });
 
   const categoryExists = await categoryRepository.findOneBy({
     name,
   });
 
+  if (!name) {
+    throw new AppError("Name required", 400);
+  }
   if (categoryExists) {
     throw new AppError("Category already exists", 400);
   }
 
-  if (!name) {
-    throw new AppError("Name is missing", 400);
-  }
-
   const category = categoryRepository.create({
     name,
-    property: property!,
   });
   await categoryRepository.save(category);
 
