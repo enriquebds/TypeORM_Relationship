bf649cfaa5da841b72b936a1a4c7bfc6976fdd99
Enrique <enrique.barbosasilva@gmail.com>
2022-10-21 15:20:49 -0300

    refactor: code-changed

--- src/controllers/categories/createCategory.controller.ts
+++ src/controllers/categories/createCategory.controller.ts
@@ -2,7 +2,8 @@ import createCategory from "../../services/categories/createCategory";
 import { Request, Response } from "express";
 
 const createCategoryController = async (req: Request, res: Response) => {
-  const name = req.body.name;
+  const { name } = req.body;
+
   const createdCategory = await createCategory(name);
   return res.status(201).json(createdCategory);
 };
