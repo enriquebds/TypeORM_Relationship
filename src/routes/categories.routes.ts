8b2ab76b737647c5290cd51d71d88aa8f2d7e2d1
Enrique <enrique.barbosasilva@gmail.com>
2022-10-22 17:18:42 -0300

    refactor: createCategory-route-changed

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -2,10 +2,16 @@ import { Router } from "express";
 import createCategoryController from "../controllers/categories/createCategory.controller";
 import listCategoryController from "../controllers/categories/listCategory.controller";
 import tokenAuthMiddleware from "../middlewares/tokenAuth.middleware";
+import verifyIsAdmMiddleware from "../middlewares/verifyIsAdm.middleware";
 
 const routes = Router();
 
-routes.post("", tokenAuthMiddleware, createCategoryController);
+routes.post(
+  "",
+  tokenAuthMiddleware,
+  verifyIsAdmMiddleware,
+  createCategoryController
+);
 routes.get("", listCategoryController);
 
 export default routes;
