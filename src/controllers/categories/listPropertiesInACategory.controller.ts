206a253756f20e3ed72f1c51139c15998c3578d9
Enrique <enrique.barbosasilva@gmail.com>
2022-10-24 19:06:57 -0300

    feat: listPropertiesInACategory-controller-created

---
+++ src/controllers/categories/listPropertiesInACategory.controller.ts
@@ -0,0 +1,9 @@
+import listPropertiesInACategory from "../../services/categories/listPropertiesInACategory";
+import { Request, Response } from "express";
+
+const listPropertiesInACategoryController = async (
+  req: Request,
+  res: Response
+) => {};
+
+export default listPropertiesInACategoryController;
