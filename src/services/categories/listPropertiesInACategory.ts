630df4f40d90c03c20b3e2eb1769c80b0d1cf49c
Enrique <enrique.barbosasilva@gmail.com>
2022-10-25 23:19:52 -0300

    feat: listPropertiesInACategory-service-created

--- src/services/categories/listPropertiesInACategory.ts
+++ src/services/categories/listPropertiesInACategory.ts
@@ -1,5 +1,24 @@
 import AppDataSource from "../../data-source";
+import { Category } from "../../entities/categories.entity";
+import { AppError } from "../../errors/appError";
 
-const listPropertiesInACategory = async () => {};
+const listPropertiesInACategory = async (id: string): Promise<Category> => {
+  const categoryRepository = AppDataSource.getRepository(Category);
+
+  const category = await categoryRepository.findOne({
+    where: {
+      id,
+    },
+    relations: {
+      properties: true,
+    },
+  });
+
+  if (!category) {
+    throw new AppError("Not found", 404);
+  }
+
+  return category;
+};
 
 export default listPropertiesInACategory;
