47e3a93d7c5e436a616d6176b8f977f873587d60
Enrique <enrique.barbosasilva@gmail.com>
2022-10-26 01:05:43 -0300

    refactor: code-channged

--- src/errors/appError.ts
+++ src/errors/appError.ts
@@ -1,7 +1,7 @@
 class AppError extends Error {
   statusCode: number;
 
-  constructor(message: string, statusCode: number) {
+  constructor(message: string, statusCode: number = 400) {
     super();
     this.message = message;
     this.statusCode = statusCode;
