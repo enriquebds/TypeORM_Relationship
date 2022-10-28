875fd256cbcd801f4f546be392c41c180556104c
Enrique <enrique.barbosasilva@gmail.com>
2022-10-28 18:28:02 -0300

    refactor: code-changed

--- src/controllers/schedules/createSchedules.controller.ts
+++ src/controllers/schedules/createSchedules.controller.ts
@@ -4,7 +4,12 @@ import { Request, Response } from "express";
 const createSchedulesController = async (req: Request, res: Response) => {
   const { date, hour, propertyId } = req.body;
   const userId = req.user.id;
-  const createdSchedule = createSchedules({ date, hour, propertyId, userId });
+  const createdSchedule = await createSchedules({
+    date,
+    hour,
+    propertyId,
+    userId,
+  });
 
   return res.status(201).json({
     schedule: createdSchedule,
