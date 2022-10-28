da22b7cef7a5b43f96b089c3969e13926ddf5949
Enrique <enrique.barbosasilva@gmail.com>
2022-10-28 18:27:25 -0300

    feat: createSchedule-service-created

--- src/services/schedules/createSchedules.ts
+++ src/services/schedules/createSchedules.ts
@@ -15,6 +15,22 @@ const createSchedules = async ({
   const propertyRepository = AppDataSource.getRepository(Property);
   const userRepository = AppDataSource.getRepository(User);
 
+  const takeAHour = Number(hour.replace(":", ""));
+
+  if (takeAHour > 1800 || takeAHour < 800) {
+    throw new AppError(
+      "It is not possible to schedule outside opening hours (opening hours from 8:00 to 18:00)",
+      400
+    );
+  }
+
+  const createDate = new Date(date);
+  const takeADay = createDate.getDay();
+
+  if (takeADay === 0 || takeADay === 6) {
+    throw new AppError("Invalid date", 400);
+  }
+
   const userExists = await userRepository.find();
   const properties = await propertyRepository.find();
 
@@ -22,6 +38,20 @@ const createSchedules = async ({
 
   const property = properties.find((e) => e.id === propertyId);
 
+  if (!property) {
+    throw new AppError("Invalid property Id", 404);
+  }
+
+  const schedules = await scheduleRepository.findOne({
+    where: {
+      date: date,
+      hour: hour,
+    },
+  });
+
+  if (schedules) {
+    throw new AppError("appointment already made", 400);
+  }
   const createdSchedule = scheduleRepository.create({
     date,
     hour,
